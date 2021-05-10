379cda75f05c344289e081d715c5b65506155ca2
GimenaMaza <62897090+GimenaMaza@users.noreply.github.com>
2021-05-10 19:45:47 -0300

    Finalizado 2°desafío

--- 2-async-typescript/main.ts
+++ 2-async-typescript/main.ts
@@ -1,5 +1,3 @@
-/* import Suma from './suma';
-import Resta from './resta';  */
 /* 
 Crear dos funciones llamadas 'operación' y 'operaciones'. 
 'operación' recibirá dos números y un string con el nombre de la operación a efectuar entre ellos: 'suma' ó 'resta' y cargará dinámicamente un módulo para realizar dicho cálculo. 
@@ -8,7 +6,6 @@ Crear dos funciones llamadas 'operación' y 'operaciones'.
 Los cálculos habilitados estarán definidos en archivos separados empleado clases donde los argumentos de entrada se guardarán en propiedades instancia privadas (#) y tendrán un método 'resultado' que hará efectiva la operación. 
 Este debe ser un proyecto que utilice typescript, async await, funciones flecha y tipado en todos sus módulos.
 */
-
 async function operacion(a: number, b: number, operacion: string){
    
     try {
@@ -17,16 +14,9 @@ async function operacion(a: number, b: number, operacion: string){
         let resultado = instanciaModulo.resultado();
         return Promise.resolve(resultado);
     } catch (error) {
-        console.error('Ocurrió un error');
+        console.error('Ocurrió un error');}
     }
 
-
-
-
-}
-
-
-// ejecuto pasando los operandos y el tipo de operacion
 async function operaciones() {
     console.log(await operacion(16, 6, 'suma'));
     console.log(await operacion(16, 6, 'resta'));
